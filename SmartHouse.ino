28c24c51fdcd22bcb8a3435168d1683ad44e8476
daocreate <daocreate@gmail.com>
2020-03-12 17:22:38 +0100

    create labrerie piches.h

--- SmartHouse.ino
+++ SmartHouse.ino
@@ -5,16 +5,23 @@
  Description:
  
 *************************************************************/
-const int PIRpin = A0;      // input to connect pirSensor
-const int Lamp1  = 13;      // output to connect Lamp1
-int PirValue = 0;           // variable to store pirSensor Value
-int DarkValue = 450  ;       // dark value to on light
+const int PIRpin    = A0;      // input to connect pirSensor
+const int Lamp1     = 13;      // output to connect Lamp1
+const int BuzzerPin = 9;      // output to connect buzzer 
 
+/* all variable store value */
+int p = 10; 
+int PirValue = 0;           // variable to store pirSensor Value
+int DarkValue = 300  ;       // dark value to on light
+
+/* define note */
+ 
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
   pinMode(PIRpin, INPUT);
   pinMode(Lamp1, OUTPUT);
+  pinMode(BuzzerPin, OUTPUT);
 
   Serial.begin(9600); //sets serial port for communication
 
@@ -31,7 +38,13 @@ void loop() {
   if (PirValue >= DarkValue)
   {
      digitalWrite(Lamp1, HIGH);
+     tone(BuzzerPin, 300, p)
+  }else
+  {
+    digitalWrite(Lamp1, LOW);
+    digitalWrite(BuzzerPin, LOW);
   }
+  
   
 
 }
